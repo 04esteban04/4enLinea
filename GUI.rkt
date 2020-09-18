82287058ad4508ec59eed48ca69d1d53897330e5
IIISAGORAIII <saulgora9@gmail.com>
2020-09-17 23:43:43 -0600

    Interfaz desarrollada

--- GUI.rkt
+++ GUI.rkt
@@ -1,7 +1,35 @@
 #lang racket
 (require racket/gui)
-  ; Make a frame by instantiating the frame% class
-  (define frame (new frame% [label "Example"]))
+
+; Make a frame by instantiating the frame% class
+  (define Main (new frame% 
+                [label "4 en Linea"]
+                [width 600]
+                [height 400]
+                [style '(fullscreen-button)]
+                [alignment '(left top)]))
+  
+  ; Make a static text message in the frame
+  
+(define Title (new message% [parent Main]
+                          [label "4 EN LINEA"]
+                          [vert-margin 10]
+                          [horiz-margin 300]))
+
+(define Tabletx (new message% [parent Main]
+                            [label "Tamaño del Tablero:"]
+                            [vert-margin 20]
+                            [horiz-margin 200]))
+
+(define Colortx (new message% [parent Main]
+                            [label "Eliga su ficha:"]
+                            [vert-margin 30]
+                            [horiz-margin 200]))
+
+
+
+
+
   
   ; Show the frame by calling its show method
-  (send frame show #t)
+  (send Main show #t)
