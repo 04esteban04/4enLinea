5d5bcbf23341d0527e8a0c669b3022dd6488f8ee
IIISAGORAIII <saulgora9@gmail.com>
2020-09-22 17:19:01 -0600

    Comenzando segunda ventana

--- GUI.rkt
+++ GUI.rkt
@@ -8,48 +8,82 @@
 ;   ||                PRIMERA VENTANA                 ||
 ;    \################################################/
 
+;FRAME DEL JUEGO
+(define gameWindow (new frame% 
+                [label "4 en Linea"]
+                [width 800]
+                [height 600]
+                [style (list 'no-resize-border)]))
+
 ;FRAME PRINCIPAL
 (define topWindow (new frame% 
                 [label "4 en Linea"]
                 [width 350]
-                [height 350]
+                [height 310]
                 [style (list 'no-resize-border)]))
+                (send topWindow show #t)
+
 
 (define(drawTitle canvas dc)
   (send dc set-font (make-object font% 20 "Century Gothic" 'decorative 'normal 'bold))
   (send dc draw-text "4 EN LINEA" 100 10))
 
 (define title(new canvas% [parent topWindow]
-                           [paint-callback
-                           (lambda (canvas dc)
-                           (send dc set-font (make-object font% 20 "Century Gothic" 'decorative 'normal 'bold))
-                           (send dc set-text-foreground "orange")
-                           (send dc draw-text "4 EN LINEA!" 100 10))]
+                           [paint-callback drawTitle]
                            [style (list 'transparent)]))
 
 ;PANEL PARA CONFIGURACIONES
 (define controlPane (new vertical-pane% [parent topWindow]
-                                           [spacing 25]))
-                           
-  
-
+                                        [spacing 25]
+                                        [alignment '(center top)]))
 ;CONFIGURACIONES PARA LA MATRIZ
-(define msg (new message% [parent controlPane]
-                          [label "Tamaño del tablero: "]))
+(new message% [parent controlPane]
+              [label "Configuraciones: "])
 
-(define selectRow(new choice% [parent controlPane]
-                               [label "Filas:  "]
-                               [choices '("8" "9" "10" "11" "12" "13" "14" "15" "16")]
-                               ))
-(define selectColumn(new choice% [parent controlPane]
-                               [label "Columnas:  "]
-                               [choices '("8" "9" "10" "11" "12" "13" "14" "15" "16")]
-                               ))
+(define sizePane (new horizontal-pane% [parent controlPane]
+                                       [spacing 5]
+                                       [alignment '(center top)]))
+                           
 
-;Boton encargado de enviar el tamaño de la matriz a la funcion respectiva
+(define selectRow(new choice% [parent sizePane]
+                              [label "Filas:  "]
+                              [choices '("8" "9" "10" "11" "12" "13" "14" "15" "16")]
+                              ))
+
+(define selectColumn(new choice% [parent sizePane]
+                                 [label "Columnas:  "]
+                                 [choices '("8" "9" "10" "11" "12" "13" "14" "15" "16")]
+                                 ))
+
+(define selectToken(new choice% [parent controlPane]
+                                [label "Ficha:  "]
+                                [choices '("Rojo" "Azul")]
+                                ))
+
+;Boton encargado de guardar las configuraciones y comenzar el juego
 (define accept(new button% [parent controlPane]
                            [label "Aceptar"]
                            [callback (lambda (button event)
-                           (createMatrix (string->number (send selectRow get-string-selection)) (string->number (send selectColumn get-string-selection))))]))
+                           (createMatrix 
+                           (string->number (send selectRow get-string-selection)) 
+                           (string->number (send selectColumn get-string-selection)))
+                           (send gameWindow show #t)
+                           (send topWindow show #f))]))
 
-(send topWindow show #t)
+
+;    /################################################\
+;   ||                 SEGUNDA VENTANA                ||
+;    \################################################/
+
+;En esta parte el juego empieza segun las configuraciones con una nueva ventana
+
+;DISEÑO
+(new canvas% [parent gameWindow]
+             [paint-callback
+             (lambda (canvas dc)
+             (send dc draw-bitmap(make-object bitmap% "3enlinea/4enLinea.png") 10 1))]
+)
+
+(define matrixPane(new pane% [parent gameWindow]
+                             [border 10]
+                             [alignment '(center center)]))
