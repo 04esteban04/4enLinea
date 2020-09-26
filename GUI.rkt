f28933a54a3a55af9f8b78f55a28e4c49b431003
IIISAGORAIII <saulgora9@gmail.com>
2020-09-25 21:28:51 -0600

    Insertar ficha segun color

--- GUI.rkt
+++ GUI.rkt
@@ -76,6 +76,8 @@
 
 ;En esta parte el juego empieza segun las configuraciones con una nueva ventana
 
+(define token (send selectToken get-string-selection))
+
 ;Panel encargado de mostrar el tablero
 (define matrixPanel (new panel% [parent gameWindow]
                                 [style (list 'border)]
@@ -89,7 +91,14 @@
     (super-new)
     (define/override (on-paint before? dc . other)
       (when before?
-        (draw-4Line-board dc)))))
+        (draw-4Line-board dc)))
+    (define/override (on-event e)
+      (define window-x (send e get-x))
+      (define window-y (send e get-y))
+      (when (eq? (send e get-event-type) 'left-down)
+          (for ([id (in-hash-keys token-piece-data)])
+          (define piece (make-token-piece id))
+          (send board insert piece window-x window-y))))))
 
 ;Funcion encargada de dibujar el tablero
 (define (draw-4Line-board dc)
@@ -121,10 +130,61 @@
                        [vertical-inset 0]
                        [editor board]))
 
-(define token-snip-class
+;###########################################
+
+(define token-piece-data
+  (hash
+   "Token" #\u2b55))
+
+(define token-piece-snip-class
   (make-object
    (class snip-class%
      (super-new)
      (send this set-classname "token-snip"))))
+
+(send (get-the-snip-class-list) add token-piece-snip-class)
+
+(define token-piece%
+  (class snip%
+    (init-field glyph font size)
+    (super-new)
+    (send this set-snipclass token-piece-snip-class)
+
+    (define/override (get-extent dc x y width height descent space lspace rspace)
+      (when width (set-box! width size))
+      (when height (set-box! height size))
+      (when descent (set-box! descent 0.0))
+      (when space (set-box! space 0.0))
+      (when lspace (set-box! lspace 0.0))
+      (when rspace (set-box! rspace 0.0)))
+
+    (define/override (draw dc x y . other)
+    (cond 
+      ((equal? "Rojo" (send selectToken get-string-selection))
+
+      (send dc set-font font)
+      (send dc set-text-foreground "red")
+      (define-values (glyph-width glyph-height baseline extra-space)
+      (send dc get-text-extent glyph font #t))
+      (let ((ox (/ (- size glyph-width) 2))
+            (oy (/ (- size glyph-height 2))))
+        (send dc draw-text glyph (+ x ox) (+ y oy))))
+      
+      ((equal? "Azul" (send selectToken get-string-selection))
+      
+      (send dc set-font font)
+      (send dc set-text-foreground "blue")
+      (define-values (glyph-width glyph-height baseline extra-space)
+      (send dc get-text-extent glyph font #t))
+      (let ((ox (/ (- size glyph-width) 2))
+            (oy (/ (- size glyph-height 2))))
+        (send dc draw-text glyph (+ x ox) (+ y oy))))
+    ))))
+        
+(define (make-token-piece id)
+  (define glyph (hash-ref token-piece-data id))
+  (define font (send the-font-list find-or-create-font 20 'default 'normal 'normal))
+  (new token-piece% [glyph (string glyph)] [font font] [size 35]))
+
 
 
